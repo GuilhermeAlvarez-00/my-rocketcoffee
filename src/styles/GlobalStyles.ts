00cad369576151acc457e55463e05ba346b9dea6
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-07-10 15:43:23 -0300

    feat: configuring global styles

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  :root {
+    --backgrond: #000;
+    --text-color: #FFF;
+    --button: #8257E5;
+    --border: #29292E;
+    --border-menu-mobile: #A8A8B3;
+    --text-color-menu-mobile: #E1E1E6;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Manrope', sans-serif;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  body {
+    color: var(--text-color);
+    background: var(--backgrond);
+  }
+`;
